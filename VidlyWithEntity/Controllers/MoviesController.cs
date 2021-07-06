e665ed5f1801204636fd4a80cc24a44a28b85a21
Rezaul-Karim-Sujon <rksujon126706@gmail.com>
2021-07-06 17:04:53 +0600

    Attribute routing(MapMvcAttributeRoutes)

--- VidlyWithEntity/Controllers/MoviesController.cs
+++ VidlyWithEntity/Controllers/MoviesController.cs
@@ -16,6 +16,15 @@ namespace VidlyWithEntity.Controllers
                 Name = "Shrek!"
             };
             return View(movie);
+            // return Content("Hello World");
+            // return HttpNotFound();
+            // return new EmptyResult();
+            // return RedirectToAction("Index", "Home",new{page=1, sortBy=name});
+        }
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
