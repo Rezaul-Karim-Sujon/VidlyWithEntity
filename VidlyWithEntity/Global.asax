dc4fb96b2d2fee3457ca4288e5abdd1ee0b473f8
Rezaul-Karim-Sujon <rksujon126706@gmail.com>
2021-07-06 16:34:48 +0600

    Add project files.

---
+++ VidlyWithEntity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VidlyWithEntity.MvcApplication" Language="C#" %>
---
+++ VidlyWithEntity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyWithEntity.Startup))]
+namespace VidlyWithEntity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
