2fa8378caf25457de357dab730d41c54d7e6d04d
Rezaul-Karim-Sujon <rksujon126706@gmail.com>
2021-07-12 10:57:19 +0600

    Create new member to database page added

--- VidlyWithEntity/Controllers/CustomersController.cs
+++ VidlyWithEntity/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using VidlyWithEntity.Models;
+using VidlyWithEntity.ViewModels;
 
 namespace VidlyWithEntity.Controllers
 {
@@ -19,12 +20,31 @@ namespace VidlyWithEntity.Controllers
         {
             _context.Dispose();
         }
-        // GET: Customers
+        // GET:Customers/New
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel
+            {
+                MembershipTypes = membershipTypes
+        };
+            return View(viewModel);
+        }
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            return RedirectToAction("Index","Customers");
+        }
+
+        // GET: Customers/Index
         public ViewResult Index()
         {
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
+        // GET:Customers/Details
         public ActionResult Details(int id)
         {
             var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
