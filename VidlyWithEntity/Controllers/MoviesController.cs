8f4377b9567b0ebeff0181e7aa5556091dbb5c6a
Rezaul-Karim-Sujon <rksujon126706@gmail.com>
2021-07-12 17:14:31 +0600

    Solution - Section 4 Exercise - Add a form to create/edit a movie.

--- VidlyWithEntity/Controllers/MoviesController.cs
+++ VidlyWithEntity/Controllers/MoviesController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using VidlyWithEntity.Models;
+using VidlyWithEntity.ViewModels;
+
 namespace VidlyWithEntity.Controllers
 {
     public class MoviesController : Controller
@@ -19,17 +21,57 @@ namespace VidlyWithEntity.Controllers
         {
             _context.Dispose();
         }
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel 
+            {
+                Genres=genres,
+            };
+            return View("MovieForm",viewModel);
+        }
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if(movie.Id==0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index","Movies");
+        }
         public ViewResult Index()
         {
-            var movies = _context.Movies.Include(c => c.Genre).ToList();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
             return View(movies);
         }
         public ActionResult Details(int id)
         {
-            var movie = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
             if (movie == null)
                 return HttpNotFound();
             return View(movie);
+        }
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+                if (movie == null)
+                return HttpNotFound();
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+            return View("MovieForm",viewModel);
         }
     }
 }
